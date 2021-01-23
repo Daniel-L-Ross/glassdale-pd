ed755010cd857c894dc6e873f0e7000e407b3aad
Dan Ross <daniellross@gmail.com>
2021-01-22 18:04:33 -0600

    added flex and basic text styling to criminal cards

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,11 +1,11 @@
 export const criminal = (convict) => {
     return `
         <div class="criminal__card">
-            <div class="crimnal__name">${convict.name}</div>
-            <div class="crimnal__age">${convict.age}</div>
-            <div class="criminal__conviction">${convict.conviction}</div>
-            <div class="criminal__term">${new Date(convict.incarceration.start).toLocaleDateString('en-US')}</div>
-            <div class="criminal__term">${new Date(convict.incarceration.end).toLocaleDateString('en-US')}</div>
+            <div class="criminal__name">${convict.name}</div>
+            <div class="crimnal__age">Age: ${convict.age}</div>
+            <div class="criminal__conviction">Crime: ${convict.conviction}</div>
+            <div class="criminal__term">Term start: ${new Date(convict.incarceration.start).toLocaleDateString('en-US')}</div>
+            <div class="criminal__term">Term end: ${new Date(convict.incarceration.end).toLocaleDateString('en-US')}</div>
         </div>
     `
 }
