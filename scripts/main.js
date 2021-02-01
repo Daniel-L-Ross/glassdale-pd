a12d441a22807b6bdc292265d83737c62c95802b
Dan Ross <daniellross@gmail.com>
2021-02-01 09:23:32 -0600

    added files for witness components. Ran fetch call on witness data and printed to the console successfully

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,13 @@ import { NoteForm } from './notes/NoteForm.js'
 import { ToggleNotesButton  } from './notes/ShowNotesButton.js'
 import './notes/NoteList.js'
 import './alibi/AlibiDisplay.js'
+import { getWitnesses, useWitnesses } from './witnesses/WitnessProvider.js'
 
 ConvictionSelect()
 OfficerSelect()
 NoteForm()
 criminalList()
 ToggleNotesButton()
+
+getWitnesses()
+.then(console.log(useWitnesses))
