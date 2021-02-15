0a5e7eed79e303f76d7b0742309498813dd932b7
Dan Ross <daniellross@gmail.com>
2021-02-15 10:46:35 -0600

    criminal list function passes correct data to render function

--- scripts/facility/FacilityList.js
+++ scripts/facility/FacilityList.js
@@ -1,8 +1,29 @@
-
+import { getCriminals, useCriminals } from '../criminals/CriminalProvider.js'
+import { getFacilities, useFacilites } from './FacilityProvider.js'
+import { getCriminalFacilities, useCriminalFacilities } from './CriminalFacilityProvider.js'
 
 const eventHub = document.querySelector(".container")
 
 
 eventHub.addEventListener("facilitiesButtonClicked", facilityEvent => {
-    console.log("Facilties button click heard")
+        facilityList()
 })
+
+
+const render = (criminalsArray, facilitiesArray, crimFacArray) => {
+    console.log(criminalsArray[0], facilitiesArray[0], crimFacArray[0])
+}
+
+const facilityList = () => {
+    getFacilities()
+    .then(getCriminalFacilities)
+    .then(getCriminals)
+    .then(() => {
+        const appStateCriminals = useCriminals()
+        const appStateFacilities = useFacilites()
+        const appStateCriminalFacilities = useCriminalFacilities()
+            render(appStateCriminals, appStateFacilities, appStateCriminalFacilities)
+    })
+    
+    console.log("facility list ran")
+}
